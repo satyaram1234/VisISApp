80fedb33d38e0663906e96993a1cc3e5457f32ed
C11206 <C11206@L-PF0VLQ2Y.hotelgroup.com>
2019-02-28 15:39:35 -0500

    POM file update

--- BeVins_id/src/main/java/TestNGDemoNike/BaseClass.java
+++ BeVins_id/src/main/java/TestNGDemoNike/BaseClass.java
@@ -3,6 +3,7 @@ package TestNGDemoNike;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.Assert;
 import org.testng.annotations.Parameters;
 import org.testng.annotations.Test;
 
@@ -16,15 +17,18 @@ public class BaseClass {
 //	String url = "http://www.google.com";
 //	String userID= "Ramanna.satyaph@testNG.com";
 	
-		@Test()
+		@Test(groups = "Base")
 		@Parameters({"browser","url","userID"})
-		public void testNGDemo(String browser, String url, String userID){
+		public void testNGDemo(String browser, String url, String userID) throws InterruptedException{
 	        if(browser.equals("chrome")){
 				 String ChromeDriverPath = "C:\\Rammana_Wyn\\2019\\Selenium\\Sel Drivers\\chromedriver.exe";
 				 System.setProperty("webdriver.chrome.driver",ChromeDriverPath);
 				 driver= new ChromeDriver();
 			     driver.get(url);
 			     driver.findElement(By.linkText("Gmail")).click();
+			     Thread.sleep(2000);
+			     Assert.assertEquals("Gmail - Free Storage and Email from Google", driver.getTitle());
+			     Assert.assertTrue( driver.findElement(By.linkText("SIGN IN")).isDisplayed());
 			     driver.findElement(By.linkText("SIGN IN")).click();
 			     driver.findElement(By.xpath(".//*[@id='identifierId']")).sendKeys(userID);
 			     driver.findElement(By.xpath("//*[@id='identifierNext']/content/span")).click();
